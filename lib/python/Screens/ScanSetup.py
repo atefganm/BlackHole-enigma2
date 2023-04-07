4a5176b30ad6b57b1dbab00b39c9b6c8cd3e7c38
atefganm <atef_anim_1974@yahoo.com>
2023-04-07 20:47:10 +0300

    [ScanSetup] Fix crash: modes = [x[:5] for x in n.getTunerTypesEnabled. TypeError: 'NoneType' object is not subscriptable Thanks to @fairbird

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -1000,7 +1000,10 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		self.nim_type_dict = {}
 		# collect all nims which are *not* set to "nothing"
 		for n in nimmanager.nim_slots:
-			modes = [x[:5] for x in n.getTunerTypesEnabled()]
+			try:
+				modes = [x[:5] for x in n.getTunerTypesEnabled()]
+			except:
+				pass
 			if n.config_mode == "nothing":
 				continue
 			if "DVB-S" in modes: # Only do the DVB-S tests below when DVB-S is present in modes. This avoids problems with combined tuners such as Availink AVL6862.
@@ -1011,6 +1014,8 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 					if n.type == nimmanager.nim_slots[root_id].type: # check if connected from a DVB-S to DVB-S2 Nim or vice versa
 						continue
 			nim_list.append((str(n.slot), n.friendly_full_description))
+
+			modes = [x[:5] for x in n.getTunerTypesEnabled()]
 			self.nim_type_dict[n.slot] = {"modes": modes,
 				"selection": ConfigSelection(choices=[(x, {"DVB-S": _("Satellite"), "DVB-T": _("Terrestrial"), "DVB-C": _("Cable"), "ATSC": _("ATSC")}.get(x, "")) for x in modes])}
 			if ttype in modes:

08d3738839b2a2495e2d307503ad72867639ca35
atefganm <atef_anim_1974@yahoo.com>
2023-07-01 11:31:11 +0300

    About.py(Components) - cleanup, check file exists rather than try/except Thanks to @Twol

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,5 +1,6 @@
 from sys import modules, version_info
 from os import path as ospath
+from time import time
 import socket
 import fcntl
 import struct
@@ -42,16 +43,16 @@ def getGStreamerVersionString():
 
 
 def getKernelVersionString():
-	try:
+	if ospath.isfile("/proc/version"):
 		with open("/proc/version", "r") as f:
 			kernelversion = f.read().split(" ", 4)[2].split("-", 2)[0]
 			return kernelversion
-	except:
+	else:
 		return _("unknown")
 
 
 def getIsBroadcom():
-	try:
+	if ospath.isfile("/proc/cpuinfo"):
 		with open("/proc/cpuinfo", "r") as file:
 			lines = file.readlines()
 			for x in lines:
@@ -67,7 +68,7 @@ def getIsBroadcom():
 			return True
 		else:
 			return False
-	except:
+	else:
 		return False
 
 
@@ -77,6 +78,7 @@ def getModelString():
 
 
 def getChipSetString():
+	if ospath.isfile("/proc/stb/info/chipset"):
 	if getMachineBuild() in ('dm7080', 'dm820'):
 		return "7435"
 	elif getMachineBuild() in ('dm520', 'dm525'):
@@ -93,13 +95,13 @@ def getChipSetString():
 			chipset = f.read()
 			f.close()
 			return str(chipset.lower().replace('\n', '').replace('bcm', '').replace('brcm', '').replace('sti', ''))
-		except IOError:
+	else:
 			return "unavailable"
 
 
 def getCPUSpeedMHzInt():
 	cpu_speed = 0
-	try:
+	if ospath.isfile("/proc/cpuinfo"):
 		with open("/proc/cpuinfo", "r") as file:
 			lines = file.readlines()
 			for x in lines:
@@ -109,11 +111,11 @@ def getCPUSpeedMHzInt():
 					if splitted[0].startswith("cpu MHz"):
 						cpu_speed = float(splitted[1].split(" ")[0])
 						break
-	except IOError:
+	else:
 		print("[About] getCPUSpeedMHzInt, /proc/cpuinfo not available")
 
 	if cpu_speed == 0:
-		if getMachineBuild() in ("h7", "hd51", "sf4008"):
+		if getMachineBuild() in ("h7", "hd51", "sf4008", "osmio4k", "osmio4kplus", "osmini4k"):
 			try:
 				import binascii
 				with open("/sys/firmware/devicetree/base/cpus/cpu@0/clock-frequency", "rb") as f:
@@ -215,8 +217,7 @@ def getCPUString():
 
 
 def getCpuCoresInt():
-	cores = 0
-	try:
+	if ospath.isfile("/proc/cpuinfo"):
 		with open("/proc/cpuinfo", "r") as file:
 			lines = file.readlines()
 			for x in lines:
@@ -225,9 +226,9 @@ def getCpuCoresInt():
 					splitted[1] = splitted[1].replace("\n", "")
 					if splitted[0].startswith("processor"):
 						cores = int(splitted[1]) + 1
-	except IOError:
-		pass
-	return cores
+						return cores
+	else:
+		return 0
 
 
 def getCpuCoresString():
@@ -283,11 +284,9 @@ def getPythonVersionString():
 
 
 def getEnigmaUptime():
-	from time import time
-	import os
 	location = "/etc/enigma2/profile"
 	try:
-		seconds = int(time() - os.path.getmtime(location))
+		seconds = int(time() - ospath.getmtime(location))
 		return formatUptime(seconds)
 	except:
 		return ''
@@ -295,9 +294,8 @@ def getEnigmaUptime():
 
 def getBoxUptime():
 	try:
-		f = open("/proc/uptime", "rb")
-		seconds = int(f.readline().split('.')[0])
-		f.close()
+		with open("/proc/uptime", "rb") as f:
+			seconds = int(f.readline().split('.')[0])
 		return formatUptime(seconds)
 	except:
 		return ''
@@ -320,23 +318,20 @@ def formatUptime(seconds):
 
 
 def getEnigmaUptime():
-	from time import time
-	import os
-	location = "/etc/enigma2/profile"
 	try:
-		seconds = int(time() - os.path.getmtime(location))
+		seconds = int(time() - ospath.getmtime("/etc/enigma2/profile"))
 		return formatUptime(seconds)
 	except:
 		return ''
 
 def getBoxUptime():
 	try:
-		f = open("/proc/uptime", "rb")
-		seconds = int(f.readline().split('.')[0])
-		f.close()
+		with open("/proc/uptime", "rb") as f:
+			seconds = int(f.readline().split('.')[0])
 		return formatUptime(seconds)
 	except:
 		return ''
+
 
 def formatUptime(seconds):
 	out = ''

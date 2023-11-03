d64c0deebc207a57f882c03b8f2ed52d211468d2
atefganm <atef_anim_1974@yahoo.com>
2023-11-03 18:47:35 +0300

    Resend file..

--- data/convert-iso-639-3.py
+++ data/convert-iso-639-3.py
@@ -8,16 +8,16 @@ if len(sys.argv) > 1:
 if len(sys.argv) > 2:
 	outfilename = sys.argv[2]
 
-l = {}
+data = {}
 
 with open(infilename, 'r') as f:
-	f.readline() # throw away header line
+	f.readline()  # throw away header line
 	for line in f:
 		item = line.split('\t')
 		name = (item[6],)
 		for i in range(4):
 			if item[i]:
-				l[item[i]] = name
+				data[item[i]] = name
 
 with open(outfilename, 'wb') as f:
-	pickle.dump(l, f, pickle.HIGHEST_PROTOCOL)
+	pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)

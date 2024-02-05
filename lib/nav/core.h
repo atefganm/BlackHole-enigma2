661aed5410a1bf1e8c416b5f31c4d9c9639ff96c
atefganm <atef_anim_1974@yahoo.com>
2024-02-06 00:32:54 +0300

    Update

--- lib/nav/core.h
+++ lib/nav/core.h
@@ -15,9 +15,6 @@ class eNavigation;
 class pNavigation: public iObject, public sigc::trackable
 {
     DECLARE_REF(pNavigation);
-
-	friend class eFCCServiceManager;
-	ePtr<eFCCServiceManager> m_fccmgr;
 public:
 	enum RecordType {
 	    isRealRecording          =     1,
@@ -46,6 +43,7 @@ public:
     void getRecordings(std::vector<ePtr<iRecordableService> > &recordings, bool simulate=false, RecordType type=isAnyRecording);
     void getRecordingsServicesOnly(std::vector<eServiceReference> &services, pNavigation::RecordType type=isAnyRecording);
     void getRecordingsTypesOnly(std::vector<pNavigation::RecordType> &services, pNavigation::RecordType type=isAnyRecording);
+    void getRecordingsSlotIDsOnly(std::vector<int> &slotids, pNavigation::RecordType type=isAnyRecording);
     std::map<ePtr<iRecordableService>, eServiceReference, std::less<iRecordableService*> > getRecordingsServices(RecordType type=isAnyRecording);
     void navEvent(int event);
 
@@ -56,44 +54,49 @@ private:
 };
 
 
-class eNavigation: public iObject, public Object
+class eNavigation: public iObject, public sigc::trackable
 {
-    static eNavigation *instance;
-    DECLARE_REF(eNavigation);
-    int m_decoder;
-    ePtr<iServiceHandler> m_servicehandler;
+	static eNavigation *instance;
+	DECLARE_REF(eNavigation);
+	int m_decoder;
+	ePtr<iServiceHandler> m_servicehandler;
 
-    ePtr<iPlayableService> m_runningService;
-    Signal1<void,int> m_event;
-    ePtr<eConnection> m_service_event_conn;
-    void serviceEvent(iPlayableService* service, int event);
+	ePtr<iPlayableService> m_runningService;
+	sigc::signal1<void,int> m_event;
+	ePtr<eConnection> m_service_event_conn;
+	void serviceEvent(iPlayableService* service, int event);
 
-    std::map<ePtr<iRecordableService>, ePtr<eConnection>, std::less<iRecordableService*> > m_recordings;
-    std::map<ePtr<iRecordableService>, eServiceReference, std::less<iRecordableService*> > m_recordings_services;
-    std::map<ePtr<iRecordableService>, pNavigation::RecordType, std::less<iRecordableService*> > m_recordings_types;
-    std::set<ePtr<iRecordableService>, std::less<iRecordableService*> > m_simulate_recordings;
+	std::map<ePtr<iRecordableService>, ePtr<eConnection>, std::less<iRecordableService*> > m_recordings;
+	std::map<ePtr<iRecordableService>, eServiceReference, std::less<iRecordableService*> > m_recordings_services;
+	std::map<ePtr<iRecordableService>, pNavigation::RecordType, std::less<iRecordableService*> > m_recordings_types;
+	std::set<ePtr<iRecordableService>, std::less<iRecordableService*> > m_simulate_recordings;
 
-    Signal2<void,ePtr<iRecordableService>,int> m_record_event;
-    void recordEvent(iRecordableService* service, int event);
+	sigc::signal2<void,ePtr<iRecordableService>,int> m_record_event;
+	void recordEvent(iRecordableService* service, int event);
+
+	friend class eFCCServiceManager;
+	ePtr<eFCCServiceManager> m_fccmgr;
 public:
 
-    RESULT playService(const eServiceReference &service);
-    RESULT connectEvent(const Slot1<void,int> &event, ePtr<eConnection> &connection);
-    RESULT connectRecordEvent(const Slot2<void,ePtr<iRecordableService>,int> &event, ePtr<eConnection> &connection);
-/*  int connectServiceEvent(const Slot1<void,iPlayableService*,int> &event, ePtr<eConnection> &connection); */
-    RESULT getCurrentService(ePtr<iPlayableService> &service);
-    RESULT stopService(void);
+	RESULT playService(const eServiceReference &service);
+	RESULT connectEvent(const sigc::slot1<void,int> &event, ePtr<eConnection> &connection);
+	RESULT connectRecordEvent(const sigc::slot2<void,ePtr<iRecordableService>,int> &event, ePtr<eConnection> &connection);
+/*	int connectServiceEvent(const sigc::slot1<void,iPlayableService*,int> &event, ePtr<eConnection> &connection); */
+	RESULT getCurrentService(ePtr<iPlayableService> &service);
+	RESULT stopService(void);
 
-    RESULT recordService(const eServiceReference &ref, ePtr<iRecordableService> &service, bool simulate, pNavigation::RecordType type);
-    RESULT stopRecordService(ePtr<iRecordableService> &service);
-    void getRecordings(std::vector<ePtr<iRecordableService> > &recordings, bool simulate, pNavigation::RecordType type);
-    void getRecordingsServicesOnly(std::vector<eServiceReference> &services, pNavigation::RecordType type);
-    void getRecordingsTypesOnly(std::vector<pNavigation::RecordType> &services, pNavigation::RecordType type);
-    std::map<ePtr<iRecordableService>, eServiceReference, std::less<iRecordableService*> > getRecordingsServices(pNavigation::RecordType type);
+	RESULT recordService(const eServiceReference &ref, ePtr<iRecordableService> &service, bool simulate, pNavigation::RecordType type);
+	RESULT stopRecordService(ePtr<iRecordableService> &service);
+	void getRecordings(std::vector<ePtr<iRecordableService> > &recordings, bool simulate, pNavigation::RecordType type);
+	void getRecordingsServicesOnly(std::vector<eServiceReference> &services, pNavigation::RecordType type);
+	void getRecordingsTypesOnly(std::vector<pNavigation::RecordType> &services, pNavigation::RecordType type);
+	void getRecordingsSlotIDsOnly(std::vector<int> &slotids, pNavigation::RecordType type);
+	std::map<ePtr<iRecordableService>, eServiceReference, std::less<iRecordableService*> > getRecordingsServices(pNavigation::RecordType type);
 
-    RESULT pause(int p);
-    eNavigation(iServiceHandler *serviceHandler, int decoder = 0);
-    static eNavigation *getInstance() { return instance; }
-    virtual ~eNavigation();
+	RESULT pause(int p);
+	eNavigation(iServiceHandler *serviceHandler, int decoder = 0);
+	static eNavigation *getInstance() { return instance; }
+	virtual ~eNavigation();
 };
+
 #endif
